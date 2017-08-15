7cdb39bf1871effb446e273d0013c6b100b86d06
Amr Arnous <amrarnous15@gmail.com>
2017-08-15 00:34:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.error('I\'m Not In Danger, I\'m The Danger')
+console.error('I\'m Not In Danger, I\'m The Danger');
+console.warn('Warn You are going deep in danger');
+console.log('Every Little thing is fine until now bro!');
